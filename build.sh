1b64d841ae2a8bc324301d272ebcdbeb9311cb5e
Zhiyu Wang <wangzy95@bu.edu>
2019-05-03 18:01:24 -0400

    readme

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 mknod /dev/mygpio c 61 0
-insmod mykernel.ko  
+insmod mykernel.ko
+
+export QWS_MOUSE_PROTO='tslib:/dev/input/touchscreen0'
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_PLUGINDIR=/usr/lib
+export TSLIB_TSDEVICE=/dev/input/event0
+export TSLIB_FBDEVICE=/dev/fb0
+export TSLIB_CONSOLEDEVICE=/dev/tty
+export QT_QWS_FONTDIR=/media/card/lib/fonts
+export TSLIB_PLUGINDIR=/usr/lib/ts
+./qt -qws
